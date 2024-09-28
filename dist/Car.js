0a130bd95a1955a7bb30c88b65e30bb7d7f180c2
Gabiconiglio <111024744+Gabiconiglio@users.noreply.github.com>
2024-09-28 19:55:49 -0300

    app correction
    app correction

--- dist/Car.js
+++ dist/Car.js
@@ -1,20 +1,19 @@
-import { Vehicle } from "./Vehicle.js";
-
+import { Vehicle } from "./Vehicle";
 export class Car extends Vehicle {
-  constructor(brand, model, maximumSpeed, numberOfDoors) {
-    super(brand, model, maximumSpeed);
-    this._numberOfDoors = numberOfDoors;
-  }
-  get numberOfDoors() {
-    return this._numberOfDoors;
-  }
-  set numberOfDoors(numberOfDoors) {
-    this._numberOfDoors = numberOfDoors;
-  }
-  accelerate(speed) {
-    console.log(`The car is accelerating to ${speed} km/h.`);
-  }
-  curb() {
-    return "the car is braking";
-  }
+    constructor(brand, model, maximumSpeed, numberOfDoors) {
+        super(brand, model, maximumSpeed);
+        this._numberOfDoors = numberOfDoors;
+    }
+    get numberOfDoors() {
+        return this._numberOfDoors;
+    }
+    set numberOfDoors(numberOfDoors) {
+        this._numberOfDoors = numberOfDoors;
+    }
+    accelerate(speed) {
+        console.log(`The car is accelerating to ${speed} km/h.`);
+    }
+    curb() {
+        return "the car is braking";
+    }
 }
--- dist/Motorbike.js
+++ dist/Motorbike.js
@@ -1,20 +1,19 @@
-import { Vehicle } from "./Vehicle.js";
-
+import { Vehicle } from "./Vehicle";
 export class MotorBike extends Vehicle {
-  constructor(brand, model, maximumSpeed, handlebarType) {
-    super(brand, model, maximumSpeed);
-    this._handlebarType = handlebarType;
-  }
-  get handlebarType() {
-    return this._handlebarType;
-  }
-  set handlebarType(handlebarType) {
-    this._handlebarType = handlebarType;
-  }
-  accelerate(speed) {
-    console.log(`The motorbike is accelerating to ${speed} km/h.`);
-  }
-  curb() {
-    return "the motorbike is braking";
-  }
+    constructor(brand, model, maximumSpeed, handlebarType) {
+        super(brand, model, maximumSpeed);
+        this._handlebarType = handlebarType;
+    }
+    get handlebarType() {
+        return this._handlebarType;
+    }
+    set handlebarType(handlebarType) {
+        this._handlebarType = handlebarType;
+    }
+    accelerate(speed) {
+        console.log(`The motorbike is accelerating to ${speed} km/h.`);
+    }
+    curb() {
+        return "the motorbike is braking";
+    }
 }
